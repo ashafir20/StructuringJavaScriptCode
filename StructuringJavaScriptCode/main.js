aac73c10726723fc6d617cd73bcb3a3d3d812699
Aviv Shafir <aviv@optimalfinance.co.il>
2015-03-21 13:18:22 +0200

    init

---
+++ StructuringJavaScriptCode/main.js
@@ -0,0 +1,17 @@
+﻿window.onload = function() {
+
+    //similiar to a class
+    function closure() {
+        var date = new Date();
+        return function () {
+            return date.getMilliseconds();
+        }
+    }
+
+    console.log(closure());
+
+    window.setTimeout(function() {
+        console.log(closure());
+    }, 500);
+
+}
