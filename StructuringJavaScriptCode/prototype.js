f63c1635280fdf5936e9c8d2e151bef282afe468
Aviv Shafir <aviv@optimalfinance.co.il>
2015-03-21 19:04:11 +0200

    finished prototype example

--- StructuringJavaScriptCode/prototype.js
+++ StructuringJavaScriptCode/prototype.js
@@ -1,14 +1,26 @@
-﻿window.onload = function() {
-    var calc = new Calculator('eq');
+﻿window.onload = function () {
+
+    //overriding the calculator's (from 3rd party) add function
+    myNamespace.Calculator.proptotype.add = function (x, y) {
+        var val = x - y;
+        this.eqCtl.innerHTML = val;
+    };
+
+    var calc = new myNamespace.Calculator('eq');
     calc.add(2, 2);
 };
 
-//prototyping
-var Calculator = function (eq) {
+
+
+//prototyping (assume source code from 3rd party library)
+//use existing namespace or create new
+var myNamespace = myNamespace || {};
+
+myNamespace.Calculator = function (eq) {
     this.eqCtl = document.getElementById(eq);
 };
 
-Calculator.proptotype = {
+myNamespace.Calculator.proptotype = {
     add: function(x, y) {
         var val = x + y;
         this.eqCtl.innerHTML = val;
