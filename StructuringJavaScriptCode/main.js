ddf6743f21262dc1d46e49ae99ef8748849ff672
Aviv Shafir <aviv@optimalfinance.co.il>
2015-03-21 18:24:32 +0200

    added prototype demo

--- StructuringJavaScriptCode/main.js
+++ StructuringJavaScriptCode/main.js
@@ -1,17 +1,42 @@
 ﻿window.onload = function() {
 
+    //Example 1
+
     //similiar to a class
-    function closure() {
+    function myClosure() {
         var date = new Date();
-        return function () {
+        return function() {
             return date.getMilliseconds();
-        }
+        };
     }
 
-    console.log(closure());
+    var closure1 = myClosure();
+    console.log(closure1());
 
     window.setTimeout(function() {
-        console.log(closure());
+        console.log(closure1());
+    }, 500);
+
+
+    //Example 2
+
+    //similiar to a class -> revealing module pattern
+    var myClosure2 = function () {
+        var date = new Date();
+        var myNestedFunc = function () {
+            return date.getMilliseconds();
+        };
+
+        return {
+            myNestedFunc: myNestedFunc
+        };
+    }
+
+    var closure2 = new myClosure2();
+    console.log(closure2.myNestedFunc());
+
+    window.setTimeout(function () {
+        console.log(closure2.myNestedFunc());
     }, 500);
 
 }
---
+++ StructuringJavaScriptCode/prototype.js
@@ -0,0 +1,16 @@
+﻿window.onload = function() {
+    var calc = new Calculator('eq');
+    calc.add(2, 2);
+};
+
+//prototyping
+var Calculator = function (eq) {
+    this.eqCtl = document.getElementById(eq);
+};
+
+Calculator.proptotype = {
+    add: function(x, y) {
+        var val = x + y;
+        this.eqCtl.innerHTML = val;
+    }
+};
