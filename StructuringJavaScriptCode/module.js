c022629e53e8368f353847c3385ee7149fb6048d
ashafir <aviv@optimalfinance.co.il>
2015-03-22 21:36:34 +0200

    added modules example

---
+++ StructuringJavaScriptCode/module.js
@@ -0,0 +1,22 @@
+﻿window.onload = function() {
+    var calc = new myNS.Calculator('eq');
+    calc.add(2, 2);
+}
+
+
+var myNS = myNS || {};
+
+//calculator.js
+//Similiar to a class container
+myNS.Calculator = function (eq) {
+    //private member
+    var eqCtl = document.getElementById(eq);
+
+    return {
+        //expose public member
+        add: function(x, y) {
+            var val = x + y;
+            eqCtl.innerHTML = val;
+        }
+    };
+};
