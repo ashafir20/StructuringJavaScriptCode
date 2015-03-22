a1a16cb3f4dec3baaabc722426f537af2c8e2f0a
ashafir <aviv@optimalfinance.co.il>
2015-03-23 00:35:41 +0200

    added reveleaing module pattern example

---
+++ StructuringJavaScriptCode/revealing-module.js
@@ -0,0 +1,25 @@
+﻿window.onload = function() {
+    calculator.add(2, 2);
+    calculator.subtract(2, 2);
+};
+
+//calculator.js
+var calculator = function (eq) {
+
+    //private members
+    var eqCtl = document.getElementById(eq),
+        add = function(x, y) {
+            eqCtl.innerHTML = x + y;
+        },
+        subtract = function(x, y) {
+            eqCtl.innerHTML = x - y;
+        };
+
+    //public API
+    //Expose public member
+    return {
+        add: add,
+        subtract: subtract
+    }; 
+
+}('Output'); //self invoked module
