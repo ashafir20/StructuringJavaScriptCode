1618dbb388bd009520861d3c50d3d444da67f2c3
ashafir <aviv@optimalfinance.co.il>
2015-03-23 23:27:12 +0200

    added examples for revealing module pattern

---
+++ StructuringJavaScriptCode/LocalStorageRevealingModuleExample.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function() {
+    var ls = new LocalStorage();
+    ls.loadSettings();
+});
+
+
+var LocalStorage = function () {
+
+};
+
+LocalStorage.prototype = function() {
+    //private members
+    var loadSettings = function() {
+
+        },
+        storeSettings = function() {
+
+        },
+        hasLocalStorage = function() {
+
+        };
+
+    //public members 
+    return {
+        loadSettings: loadSettings,
+        storeSettings: storeSettings,
+        hasLocalStorage: hasLocalStorage
+    };
+}();
---
+++ StructuringJavaScriptCode/revealing-protoype.js
@@ -0,0 +1,46 @@
+﻿window.onload = function () {
+
+    //extending the calculator (override the add method)
+    Calculator.proptotype.add = function(x, y) {
+        this.eqCtl.innerHTML = x * y;
+    };
+
+
+    var calc = new Calculator('Output');
+    calc.add(2, 2);
+    calc.subtract(2, 2);
+};
+
+
+//Calculator.js library
+var Calculator = function (eq) {
+    //State and private members
+    this.eqCtl = document.getElementById(eq);
+};
+
+Calculator.proptotype = function () {
+    //private members
+    var add = function(x, y) {
+            var val = x + y;
+            this.eqCtl.innerHTML = val;
+        },
+        subtract = function(x, y) {
+            var val = x - y;
+            this.eqCtl.innerHTML = val;
+        },
+        setVal = function(val) {
+            this.crreNumberCtl.innerHTML = val;
+        },
+        clearNumbers = function() {
+            setVal.call(this, '0'); //passing the this context to setVal method (so now setVal can use the 'this' keyeword)
+            //instead of passing 'this' context as a paramater we make it more clean
+            //instead of setVal('0', this);
+            //and setVal = function(val, thisObj) {}
+        };
+
+    //public members
+    return {
+        add: add,
+        subtract: subtract
+    };
+}(); //self invoke
